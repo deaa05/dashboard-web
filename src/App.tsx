32a88623ff625e76671e8d4dd54a2db3423e4273
deaa05 <davidalban@uees.edu.ec>
2026-05-27 17:45:52 -0500

    Clase 1 de react

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { Grid } from '@mui/material';
+import './App.css'
+
+function App() {
+    return (
+        <Grid container spacing={5} justifyContent="center" alignItems="center">
+         {/* Encabezado */}
+         <Grid  size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }} >Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+      </Grid>
+   );
+}
+
+export default App;
