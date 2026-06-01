44b9105e8e896ff3594c6314d17e6d2eb9793a38
deaa05 <davidalban@uees.edu.ec>
2026-06-01 17:14:30 -0500

    clase 2 despligue

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 import './App.css'
 
 function App() {
     return (
         <Grid container spacing={5} justifyContent="center" alignItems="center">
          {/* Encabezado */}
-         <Grid  size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid  size={{ xs: 12, md: 12 }}>
+           <HeaderUI />
+         </Grid>
 
          {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+         <Grid size={{ xs: 12, md: 12 }}container justifyContent="center" alignItems="center">
+           <AlertUI description="Esta es una alerta de ejemplo." />
+         </Grid>
 
          {/* Selector */}
          <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
