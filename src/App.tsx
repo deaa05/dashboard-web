d8e95b6a2b0a0bf4e769317489c0791e42855d59
deaa05 <davidalban@uees.edu.ec>
2026-06-08 15:20:30 -0500

    Diseño de la interfaz

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,16 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from './assets/vite.svg'
 //mport heroImg from './assets/hero.png'
-import { Grid } from '@mui/material';
-import HeaderUI from './components/HeaderUI';
-import AlertUI from './components/AlertUI';
-import './App.css'
+//import { Grid } from '@mui/material';
+//import HeaderUI from './components/HeaderUI';
+//import AlertUI from './components/AlertUI';
+//import './App.css'
+//import { useState } from "react";
+
+import DashboardUI from './components/DashboardUI';
 
 function App() {
-    return (
-      <Grid container spacing={5} sx={{ justifyContent: 'center', alignItems: 'center' }}>
-         {/* Encabezado */}
-         <Grid  size={{ xs: 12, md: 12 }}>
-           <HeaderUI />
-         </Grid>
-
-         {/* Alertas */}
-         <Grid container size={{ xs: 12, md: 12 }} sx={{ justifyContent: 'center', alignItems: 'center' }}>
-           <AlertUI description="Esta es una alerta de ejemplo." />
-         </Grid>
-
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
-
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
-
-         {/* Gráfico */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
-
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }} >Elemento: Tabla</Grid>
-
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
-
-      </Grid>
-   );
+  return <DashboardUI />;
 }
 
 export default App;
